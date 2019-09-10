870d2cf0ed143ece7a3c2d33f7757d8410bd6391
Yui Hirasawa <10993186+akiyamn@users.noreply.github.com>
2019-09-10 14:07:32 +1000

    What the fuck

--- asteroids.js
+++ asteroids.js
@@ -38,6 +38,7 @@ function asteroids() {
     }
     const controls = Observable
         .fromEvent(document, "keypress")
+        .filter(_ => !gameOver)
         .filter(event => !event.repeat)
         .map(event => event.key);
     const forward = (t) => ({
@@ -53,18 +54,38 @@ function asteroids() {
     controlElement(g, controls, k => k == "a", left);
     controlElement(g, controls, k => k == "s", backward);
     controlElement(g, controls, k => k == "d", right);
+    const bullets = [];
     function shootFrom(elem, bulletTimeout, velocityFunc) {
         const bullet = new Elem(svg, 'circle')
-            .attr("r", "3")
+            .attr("r", bulletSize.toString())
             .attr("fill", "#fff")
             .attr("transform", "translate(0 0) rotate(0)");
-        const deleteBullet = Observable.interval(bulletTimeout);
-        deleteBullet.subscribe(_ => bullet.attr("visibility", "hidden"));
+        let liveBullet = true;
+        bullets.push(bullet);
+        const bulletDeath = Observable.interval(bulletTimeout);
+        bulletDeath.subscribe(_ => {
+            if (liveBullet) {
+                deleteBullet(bullet);
+            }
+            liveBullet = false;
+        });
         const startingPos = position(elem);
         transformElement(bullet, _ => startingPos);
         const velocity = Observable.interval(1000 / FPS)
-            .takeUntil(deleteBullet)
+            .takeUntil(bulletDeath)
             .subscribe(_ => transformElement(bullet, velocityFunc));
+    }
+    let gameOver = false;
+    function killPlayer(ship) {
+        ship.attr("visibility", "hidden");
+        gameOver = true;
+        new Elem(svg, "text")
+            .attr("x", "50%")
+            .attr("y", "50%")
+            .attr("text-anchor", "middle")
+            .attr("fill", "#fff")
+            .attr("style", "font: bold 40px sans-serif;")
+            .elem.innerHTML = "GAME OVER";
     }
     const bulletMovement = (t) => ({
         x: ((t.x + (Math.sin(rad(t.rot)) * 8))),
@@ -74,8 +95,9 @@ function asteroids() {
     const shoot = controls
         .filter(k => k == " ")
         .subscribe(_ => {
-        shootFrom(g, 1000, bulletMovement);
+        shootFrom(g, 90 * 1000 / FPS, bulletMovement);
     });
+    const asteroidDivergence = 0.8;
     function spawnAsteroid(radius, initPos, velocityFunc, color) {
         color = color === undefined ? "#000" : color;
         const asteroid = new Elem(svg, 'circle')
@@ -84,29 +106,58 @@ function asteroids() {
             .attr("style", "stroke:white;stroke-width:5")
             .attr("transform", "translate(0 0) rotate(0)");
         transformElement(asteroid, _ => initPos);
+        let deleted = false;
         const controller = Observable.interval(1000 / FPS);
         controller.subscribe(_ => {
-            transformElement(asteroid, velocityFunc);
-            console.log(collideWithShip(asteroid, radius));
+            if (!deleted) {
+                transformElement(asteroid, velocityFunc);
+                if (collidedWithShip(asteroid, radius)) {
+                    killPlayer(ship);
+                }
+                const bullet = gotShot(asteroid, radius);
+                if (bullet) {
+                    deleted = true;
+                    deleteBullet(bullet);
+                    asteroidCount--;
+                    if (radius >= 16) {
+                        spawnAsteroid(radius / 2, position(asteroid), t => velocityFunc({ x: t.x - asteroidDivergence, y: t.y + asteroidDivergence, rot: 0 }), color);
+                        spawnAsteroid(radius / 2, position(asteroid), t => velocityFunc({ x: t.x + asteroidDivergence, y: t.y - asteroidDivergence, rot: 0 }), color);
+                    }
+                    asteroid.elem.remove();
+                }
+            }
         });
     }
     let asteroidCount = 0;
+    const maxAsteroids = 2;
+    const bulletSize = 3;
+    const asteroidSpawnInterval = 1000;
+    function deleteBullet(bullet) {
+        const index = bullets.indexOf(bullet);
+        console.log(index);
+        if (index >= 0) {
+            bullets.splice(bullets.indexOf(bullet), 1);
+        }
+        bullet.attr("visibility", "hidden");
+    }
     const distance = (u, v) => Math.sqrt(Math.abs((v.x - u.x) ** 2 + (v.y - u.y) ** 2));
     const elemType = (elem) => elem.elem.tagName;
-    const collision = (elem1, radius1) => (elem2, radius2) => distance(position(elem1), position(elem2)) < radius1 + radius2;
-    const collideWithShip = collision(g, 10);
-    const asteroidSpawner = Observable.interval(1000)
+    const collision = (elem1, radius1) => (elem2, radius2) => distance(position(elem1), position(elem2)) <= radius1 + radius2;
+    const collidedWithShip = collision(g, 10);
+    const gotShot = (object, radius) => bullets
+        .filter(bullet => collision(object, radius)(bullet, bulletSize))[0];
+    Observable.interval(asteroidSpawnInterval)
         .map(_ => ({
         size: Math.random() * 100,
         xPos: (Math.random() * 10000) % SCREEN_MARGIN,
-        xVelocity: 5 - Math.random() * 10,
-        yVelocity: 5 - Math.random() * 10
+        xVelocity: 4 - Math.random() * 8,
+        yVelocity: 4 - Math.random() * 8
     }))
-        .filter(rand => rand.size <= 40)
+        .filter(rand => rand.size > 8 && rand.size < 45)
         .subscribe(rand => {
-        if (asteroidCount < 5) {
+        if (asteroidCount < maxAsteroids) {
             asteroidCount++;
-            spawnAsteroid(5 + rand.size, { x: rand.xPos, y: SCREEN_MARGIN * -(rand.xPos % 2), rot: 0 }, t => ({ x: rand.xVelocity + t.x, y: rand.yVelocity + t.y, rot: 0 }), `hsl(${Math.abs(rand.xVelocity) * Math.abs(rand.yVelocity) * (360 / 25)}, 100%, 50%)`);
+            spawnAsteroid(rand.size, { x: rand.xPos, y: SCREEN_MARGIN * -(rand.xPos % 2), rot: 0 }, t => ({ x: rand.xVelocity + t.x, y: rand.yVelocity + t.y, rot: 0 }), `hsl(${Math.abs(rand.xVelocity) * Math.abs(rand.yVelocity) * (360 / 16)}, 100%, 50%)`);
         }
     });
 }
